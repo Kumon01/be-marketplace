18df405938283ec71d315dfd0530a67cd9cb874b
Kumon01 <ibnupersib27@gmail.com>
2025-01-23 20:52:08 +0700

    banner

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BannerController;
 use App\Http\Controllers\ProductTypeConroller;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
@@ -7,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('product-types', ProductTypeConroller::class);
 Route::resource('product', ProductController::class);
+Route::resourse('banner', BannerController::class);
 Route::post('product-update/{id}' , [ProductController::class, 'update']);
+Route::post('banner-update/{id}' , [BannerController::class, 'update']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
